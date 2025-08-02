8ac044f30db1d25080ebd26e99e37379b8975f51
Hugop2san <hugopereira2santos60@gmail.com>
2025-08-02 06:48:24 -0300

    treino com insercoes usando gethashcode, equals e ienumerable

--- Program.cs
+++ Program.cs
@@ -8,42 +8,62 @@ namespace treino_curso_nelio
     {
         static void Main(string[] args)
         {
-            HashSet<Produto> produto1 = new HashSet<Produto>();
-
-            produto1.Add(new Produto("TV", 1500.00));
-            produto1.Add(new Produto("SOM", 500.00));
-
-            HashSet<Ponto> j = new HashSet<Ponto>();
-
-            j.Add(new Ponto(55, 10));
-            j.Add(new Ponto(90, 4));
 
 
-            Produto produto2 = new Produto( "TV", 1500.00);
 
-            Console.WriteLine(  produto1.Contains(produto2) );
+            HashSet<Aluno> alunoscursoA = new HashSet<Aluno>( );
+
+           Console.WriteLine("how many students for the course A ?");
+            int pergunta1= int.Parse(Console.ReadLine( ));
+
+            for(int i=0; i <pergunta1 ; i++)
+            {
+                Console.Write($"Matricula estudante n°{i+1} :");
+                alunoscursoA.Add( new Aluno (int.Parse(Console.ReadLine( ))) );
+            }
+
+            
+
+
+            Console.WriteLine("how many students for the course B ?");
+            int pergunta2= int.Parse(Console.ReadLine( ));
+
+            for(int i=0; i <pergunta2 ; i++)
+            {
+                Console.Write($"Matricula estudante n°{i+1} :");
+                alunoscursoA.Add( new Aluno (int.Parse(Console.ReadLine( ))) );
+            }
+
+            
+
+
+            Console.WriteLine("how many students for the course C ?");
+            int pergunta3= int.Parse(Console.ReadLine( ));
+
+            for(int i=0; i <pergunta3 ; i++)
+            {
+                Console.Write($"Matricula estudante n°{i+1} :");
+                alunoscursoA.Add( new Aluno (int.Parse(Console.ReadLine( ))) );
+            }
+
+            
+            Console.WriteLine($"Total matriculas sem repetir: {alunoscursoA.Count()}"  );
+            //MainPrintaiai(alunoscursoA);
+            
+
+
+
 
         }
 
         static void MainPrintaiai<T>(IEnumerable<T> argumento)
         {
+            //Console.WriteLine("Matriculas :");
+            
             foreach (T j in argumento)
             {
                 Console.WriteLine(j);
             }
         }
     }
-
-
-    struct Ponto
-    {
-
-        public int Pontoa{ get; set; }
-        public int Pontob { get; set; }
-
-        public Ponto(int pontoa, int pontob)
-        { Pontoa = pontoa; Pontob = pontob; }
-    
-    }
-
 }
